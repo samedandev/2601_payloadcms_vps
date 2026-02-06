2971b40ffe47ad9b9c5b4eb1537fbf03f0b8d664
John Doe <johndoe@example.com>
2026-02-06 13:22:23 +0100

    VPS changes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 import redirects from './redirects.js'
 
+// # Version changed for VPS hosting
+
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
   : undefined || process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
